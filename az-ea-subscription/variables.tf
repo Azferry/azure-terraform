fc5eebd2f3eefa7c2950d5ee6d5d7653f1cfd17b
Ryan Ferry <ryanferry@microsoft.com>
2022-05-03 10:44:45 -0400

    Vars

--- az-ea-subscription/variables.tf
+++ az-ea-subscription/variables.tf
@@ -12,3 +12,18 @@ variable "Subscription_Name" {
   type    = string
   default = "EA Subscription"
 }
+
+variable "Management_GroupName" {
+  type    = string
+  default = "ManagementGroupName"
+}
+
+variable "RoleName" {
+  type    = string
+  default = "Reader"
+}
+
+variable "PrincipalId" {
+  type    = string
+  default = ""
+}
